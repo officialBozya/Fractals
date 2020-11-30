e7a074f9c9218ffdb98964bf2a32427de5d3d116
officialBozya <anjik.dz@gmail.com>
2020-12-01 00:16:23 +0200

    ааа, я завтра дороблю

--- Fractals/Fractals/IfsFractals.xaml.cs
+++ Fractals/Fractals/IfsFractals.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Syncfusion.SfPicker.XForms;
+using Syncfusion.XForms.Buttons;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,10 +13,18 @@ namespace Fractals
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class IfsFractals : ContentPage
     {
+        private SfPicker picker;
         public IfsFractals()
         {
             InitializeComponent();
+            picker = Content.FindByName<SfPicker>("PresetPicker");
             Content.FindByName<Button>("BackButton").Clicked += ToStart;
+            Content.FindByName<SfButton>("Popup").Clicked += OpenPopup;
+        }
+
+        private void OpenPopup(object sender, EventArgs e)
+        {
+            picker.IsOpen = true;
         }
 
         private void ToStart(object sender, EventArgs e)
